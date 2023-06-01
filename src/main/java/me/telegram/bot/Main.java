15156f2ad2de75cc3bac1e6f550caa6d6a990380
Denis_web_dev <denis13.2011@mail.ru>
2023-06-01 22:37:56 +0300

    tg bot

--- src/main/java/me/telegram/bot/Main.java
+++ src/main/java/me/telegram/bot/Main.java
@@ -3,9 +3,9 @@ package me.telegram.bot;
 
 public class Main {
 
-    private static final String BOT_TOKEN = "6180918332:AAFbG8FUGiNvwU9Adi8MAJAsyHZoQNOWtOo";
+    private static final String BOT_TOKEN = "6180918332:AAFbG8FUGiNvwU9Adi8MAJAsyHZoQNOWtOo"; //Токен ключ телеграмм бота
 
-    private static final String PROVIDER_TOKEN = "401643678:TEST:aec55839-9338-4c0c-9e78-355d0a7c589f";
+    private static final String PROVIDER_TOKEN = "401643678:TEST:aec55839-9338-4c0c-9e78-355d0a7c589f"; //Токен ключ от Сбербанка для платёжной системы
 
     public static void main(String[] args) {
         TelegramBotApplication application = TelegramBotApplication.builder()
